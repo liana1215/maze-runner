22ec4b71a95d468f2ac97fea045f4adc729d1889
tasuku <tasuku@gmail.com>
2017-01-22 22:28:37 +0900

    updated

--- maze/maze-graphic.cpp
+++ maze/maze-graphic.cpp
@@ -2,33 +2,40 @@
 
 using namespace MazeApp;
 
-int main()
+int 
+main(int argc, char** argv)
 {
-    auto maze_dim = 10;
-    std::cout << "Input size of maze: ";
-    std::cin >> maze_dim;
-    MazeBuilder builder = MazeBuilder(maze_dim);
-    MazeSolver solver = MazeSolver(maze_dim);
-    MazeDrawer drawer = MazeDrawer(32, 16);
-
-    builder.buildMaze();
-    builder.runAldousBroder();
-    
-    MazeSolver::Path solved = solver.solveMazeBFS(builder);
-    std::vector<MazeBuilder::Vertex> shortest_path;
-    auto stack_length = solved.p.size();    
-    std::cout << std::endl;
-    std::cout << "Maze solved: ";
-    std::cout << "GOAL: ";
-    for (auto i = 0; i < stack_length; i++) { 
-        MazeBuilder::Vertex v = solved.p.top(); 
-        shortest_path.push_back(v);
-        std::cout << v.number << " <-";
-        solved.p.pop();
+    if (argc == 1) {
+        std::cerr << "Input size of desired maze as parameter.\n";
+        std::exit(1);
     }
-    std::cout << "START" << std::endl;
+    else {
+        auto maze_dim = stoi(argv[1]);
+        
+        MazeBuilder builder = MazeBuilder(maze_dim);
+        MazeSolver solver = MazeSolver(maze_dim);
+        MazeDrawer drawer = MazeDrawer(32, 16);
 
-    drawer.drawMaze(builder, solver, shortest_path);
-    return 0;
+        builder.buildMaze();
+        builder.runAldousBroder();
+        
+        MazeSolver::Path solved = solver.solveMazeBFS(builder);
+        std::vector<MazeBuilder::Vertex> shortest_path;
+        auto stack_length = solved.p.size();    
+
+        std::cout << std::endl;
+        std::cout << "Maze solved: ";
+        std::cout << "GOAL: ";
+        for (auto i = 0; i < stack_length; i++) { 
+            MazeBuilder::Vertex v = solved.p.top(); 
+            shortest_path.push_back(v);
+            std::cout << v.number << " <-";
+            solved.p.pop();
+        }
+        std::cout << "START" << std::endl;
+
+        drawer.drawMaze(builder, solver, shortest_path);
+        return 0;
+    }
 }
 
